7509ab73677eaa0c07160e01b51c9aeac29d7a5a
Prathameshworks247 <prathameshworks247@gmail.com>
2026-02-22 18:45:31 +0530

    opecv practice

--- Module1/homework.py
+++ Module1/homework.py
@@ -2,7 +2,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 def detect(gray,frame):
     faces = face_cascade.detectMultiScale(gray,1.3,5)
     for (x,y,w,h) in faces:
@@ -12,6 +12,9 @@ def detect(gray,frame):
         eyes = eye_cascade.detectMultiScale(roi_gray,1.1,3)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray,1.7,21)
+        for (sx,sy,sw,sh) in smiles:
+            cv2.rectangle(roi_color,(sx,sy), (sx+sw,sy+sh), (0,0,255), 2)
     return frame
 
 video_capture = cv2.VideoCapture(0) #0 is the default camera,1 is the second camera, etc.
@@ -27,5 +30,3 @@ video_capture.release()
 cv2.destroyAllWindows()
     
     
-
-
---
+++ openCV/lesson1.py
@@ -0,0 +1,4 @@
+import cv2
+
+img = cv2.imread('data/image.jpeg')
+print(img.shape)
